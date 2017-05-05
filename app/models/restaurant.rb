1db41cde5cac3918aa10c4e6eb3995909ace149a
loic cessot <loic.cessot@gadz.org>
2017-05-05 17:42:07 +0200

    crud in progress

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,8 @@
 class Restaurant < ApplicationRecord
+  CATEGORIES = %w(chinese viet italian spanish french)
+
+  validates :stars, inclusion: { in: [1,2,3,4], allow_nil: false }
+  validates :name, uniqueness: true, presence: true
+  validates :address, presence: true
+  validates :category, presence: true, inclusion: { in: CATEGORIES }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,47 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#require 'faker'
+CATEGORIES = %w(chinese viet italian spanish french)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         "Epicure au Bristol",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+    phone_number: "0610112209",
+    category:     "italian",
+    stars:        3
+  },
+  {
+    name:         "La truffière",
+    address:      "4 rue Blainville 75005 Paris",
+    phone_number:  "0615061635",
+    category:     "viet",
+    stars:        4
+  },
+  {
+    name:         "Le pré catelan",
+    address:      "route de Suresnes 75016 Paris",
+    phone_number: "0450231156",
+    category:     "chinese",
+    stars:        2
+  }
+]
+
+puts 'Creating 20 fake restaurants à l\'ancienne...'
+20.times do
+  restaurants_attributes << {
+    name:    Faker::Company.name,
+    address: "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    phone_number: Faker::Number.number(10),
+    category: CATEGORIES.sample,
+    stars: [1, 2, 3].sample
+  }
+end
+
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
+puts "Creating #{restaurants_attributes.count} fake restaurants at end..."
