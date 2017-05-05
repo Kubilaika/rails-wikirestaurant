dd03eae19518f63fe7ace132a9915ea165a751da
loic cessot <loic.cessot@gadz.org>
2017-05-05 16:24:17 +0200

    adding stars to restaurants

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170505140318) do
+ActiveRecord::Schema.define(version: 20170505141453) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170505140318) do
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "stars"
   end
 
 end
